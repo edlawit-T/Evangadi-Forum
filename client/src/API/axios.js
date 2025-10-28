b828d8255076d220bfce59c93434a0b1271c3682
Edlawit Taye <edlawittaye1@gmail.com>
2025-10-28 21:18:41 +0300

    update base URL

--- client/src/API/axios.js
+++ client/src/API/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5500/api",
+  baseURL:
+    import.meta.env.VITE_API_URL || "https://evangadi-forum-xi.vercel.app/api",
   timeout: 10000,
 });
 
